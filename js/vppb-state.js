6f971a1d231d07bc001f54067ad84138d9b5846f
tibotlse31 <thibautgely@gmail.com>
2026-04-18 16:17:45 +0200

    Create vppb-state.js

---
+++ js/vppb-state.js
@@ -0,0 +1,29 @@
+window.VPPBState = {
+  activeModule: 'home',
+
+  basicsTopic: 'anatomy',
+
+  biomechanics: {
+    canal: '',
+    side: '',
+    mechanism: '',
+    scenario: ''
+  },
+
+  interpret: {
+    tab: 'tests',
+    selectedTest: 'dix_hallpike',
+    selectedTestSide: 'right',
+    selectedManeuver: 'epley',
+    selectedManeuverSide: 'right'
+  },
+
+  reasoning: {
+    nystagmusFamily: '',
+    latency: '',
+    duration: '',
+    reversal: '',
+    intensitySide: '',
+    selectedCaseId: 'case_1'
+  }
+};
